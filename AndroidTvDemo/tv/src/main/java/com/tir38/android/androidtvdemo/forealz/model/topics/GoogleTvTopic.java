4d7a23833b628de3056e3296d65da3d243d8e9d1
Jason Atwood <jatwood@bignerdranch.com>
2014-11-05 18:01:43 -0500

    updates model

--- AndroidTvDemo/tv/src/main/java/com/tir38/android/androidtvdemo/forealz/model/topics/GoogleTvTopic.java
+++ AndroidTvDemo/tv/src/main/java/com/tir38/android/androidtvdemo/forealz/model/topics/GoogleTvTopic.java
@@ -5,6 +5,7 @@ import com.tir38.android.androidtvdemo.forealz.model.Topic;
 
 public class GoogleTvTopic extends Topic {
     public GoogleTvTopic() {
-        super("Google TV vs. AndroidTV", 44, R.string.todo);
+        super("Google TV vs. AndroidTV", 44, R.string.google_tv_description);
+        setImageUrl("google_tv.jpg");
     }
 }
